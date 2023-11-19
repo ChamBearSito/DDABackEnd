0bdcfa48fe2f486127d98d050ac60dd308552264
Enzo Pontet <enzopch2022@gmail.com>
2023-11-18 22:02:50 -0300

    SE AGREGO CONFIG PARA CORS

---
+++ src/main/java/com/ob/ob/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.ob.ob.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Permitir solicitudes desde el origen de tu aplicación React
+        config.addAllowedOrigin("http://localhost:3000");
+
+        // Permitir métodos como GET, POST, etc.
+        config.addAllowedMethod("*");
+
+        // Permitir ciertos encabezados
+        config.addAllowedHeader("*");
+
+        // Permitir incluir cookies en las solicitudes (si es aplicable)
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
